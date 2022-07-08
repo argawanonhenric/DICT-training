0d790be64da7f742835dfca58134b2e6db37997c
argawanonhenric <argawanonhenric@gmail.com>
2022-07-08 18:33:24 +0800

    Logic Operations

---
+++ Python Programming for Beginners/Dict_Coding Exercise 3.12.py
@@ -0,0 +1,20 @@
+#Coding Exercise 3.12
+
+my_age = 22
+mom_age = 61
+sister_age = 29
+
+print(mom_age < sister_age and my_age == 22)
+# Prints False
+
+print(mom_age == 61)
+# Prints True
+
+print(mom_age > 34 or sister_age == 22)
+# Prints True
+
+print(mom_age >= 54)
+# Prints True
+
+print(not(sister_age <=400, my_age == 22))
+# Prints False
