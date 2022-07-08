ec06bd83c1357919d2eb60001ae634badbc69caf
argawanonhenric <argawanonhenric@gmail.com>
2022-07-08 18:26:01 +0800

    if / debugging

---
+++ Python Programming for Beginners/Dict Coding Exercise 1.14.py
@@ -0,0 +1,12 @@
+print("Roses are red, violets are blue.")
+# Parenthesis and Quotation Marks are added
+
+if 10 > 7:
+    print("Ten is greater than seven!")
+if 16 < 42:
+    print("Sixteen is less than forty-two!")
+# Indentations were added to indicate new blocks of code
+
+
+print("A long time ago, in a Galaxy far, far away. . .")
+# Semicolon was removed
