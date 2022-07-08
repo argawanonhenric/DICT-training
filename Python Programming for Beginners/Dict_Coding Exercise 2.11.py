d7f016c91a7503f3e9197fd482545c3545e964a1
argawanonhenric <argawanonhenric@gmail.com>
2022-07-08 18:27:12 +0800

    Data types

---
+++ Python Programming for Beginners/Dict_Coding Exercise 2.11.py
@@ -0,0 +1,14 @@
+print(10 > 7)
+#Prints True
+
+print(str(73911))
+#Prints 73911
+
+print(tuple("Thank God it's Friday!"))
+#Prints ('T', 'h', 'a', 'n', 'k', ' ', 'G', 'o', 'd', ' ', 'i', 't', "'", 's', ' ', 'F', 'r', 'i', 'd', 'a', 'y', '!')
+
+print(float(4302))
+#Prints 4302
+
+print(int(3299.35640))
+#3299
