20b3259cb3f57c70046bd6d7107e358da4b8b7ae
argawanonhenric <argawanonhenric@gmail.com>
2022-07-08 18:34:02 +0800

    arithmetic operations

---
+++ Python Programming for Beginners/DICT_Coding Exercise 3.8.py
@@ -0,0 +1,16 @@
+#Coding Exercise 3.8
+
+print(217 * 6)
+# Prints 1302
+
+print(600 + 35234)
+# Prints 35834
+
+print(67 // 12)
+# Prints 5
+
+print(56329 % 982)
+# Prints 355
+
+print(34 ** 5)
+#Prints 45435424
