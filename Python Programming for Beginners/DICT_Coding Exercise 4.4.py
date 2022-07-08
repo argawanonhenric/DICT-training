da63fc44c4a67f8871714d80ce7578ebbc9c1a32
argawanonhenric <argawanonhenric@gmail.com>
2022-07-08 18:34:31 +0800

    conditional statement

---
+++ Python Programming for Beginners/DICT_Coding Exercise 4.4.py
@@ -0,0 +1,12 @@
+# Coding Exercises if, else , elif
+x = 332
+y = 2031
+
+if x >= y:
+    print("x is greater than y")
+
+elif x == y:
+    print("x is equal to y")
+
+else:
+    print("x is less than y")
